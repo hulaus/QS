a99f26443605731d26e3c213a3413ee602d565c0
Jo Carr <hulaus21@gmail.com>
2023-09-05 09:00:45 -0700

    problem - in the scratch.py file we want to collect a name and create a greeting returning the first letter of their name

---
+++ scratch.py
@@ -0,0 +1,7 @@
+print("Hello, \nWorld")
+print("Hello, World\n\nHello World!")
+
+#Mistake Number 1, make sure to include quotation marks in your parenthesis ! 
+name = input("What is your name? " )
+
+greeting = print("Hello " + "" + name[:1])
