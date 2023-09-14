d8c7dadc6c9ebff0110f22e0cf0bd8d17900a55b
Jo Carr <hulaus21@gmail.com>
2023-09-13 23:53:01 -0700

    final saves for 9/13/2023. Starting on random number game. Got it to print out but working on displaying whether the guess is lower or higher than the number, Loop until they get the correct answer and show how far the number is from the guess

--- numbergame.py
+++ numbergame.py
@@ -2,4 +2,19 @@
 from random import seed  
 from random import randint
 
-number = randint(1, 10)
+number = randint(10, 100)
+
+input("What number do you think it is?  ")
+
+if str(input) > str(number):
+    print("Doh ! That's Too High !")
+else:
+    str(input) < str(number)
+    print("Doooohhhh ! That number is too low !")
+print(number)
+
+
+#Once running add more to the game !
+#-- Display whether the guess is higher or lower than the number --
+#-- Loop until they get the correct answer --
+#-- Show how far away the number is from the guess
