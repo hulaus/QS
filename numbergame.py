3540fdd042c4c49c669e9e4ff7b57c57ec29d4ae
Jo Carr <hulaus21@gmail.com>
2023-09-16 00:26:47 -0700

    added basic version of trying to looop through the question until you get to the right answer but running into bug where it will print the inputted number then the real number at the end of the loop and it will not wait for us to find out the right answer

--- numbergame.py
+++ numbergame.py
@@ -5,21 +5,37 @@ from random import randint
 number = randint(1, 10)
 
 userguess = input("What number do you think it is?  ")
+tryagain = input("I'll Give you another chance. What do you think the number is?  ")
+toohigh = input("Try again ! *Hint: Try a little lower maybe?  ")
+toolow = input("Try again ! *Hint: Try a little higher maybe?  ")
 
+if userguess != number:
+    print(tryagain)
+    if str(tryagain) > str(number):
+        print(toohigh)
+    elif str(tryagain) < str(number):
+        print(toolow)
+    elif str(tryagain) == str(number):
+         print("What?! Are you a mind reader? how did you get it right?")
+         print(number)
 
-if userguess > str(number):
-    print("Doh ! That's Too High !")
-    input("Try Again! What is the number?  ")
-    while userguess > str(number):
-        print("Try again")
-        tryagain = input("Cmon.. You can do it ! What is your new guess?  ")
-        print(tryagain)
-elif userguess < str(number):
-    print("Doooohhhh ! That number is too low !")
-    print(number)
-elif userguess == str(number):
-    print("Are you a mind reader?!")
-    print(number)
+#if userguess > str(number):
+#    print("Doh ! That's Too High !")
+#    input("Try Again! What is the number?  ")
+#    while userguess > str(number):
+#        print("Wah Wah... To High..Try again")
+#        tryagain = input("Cmon.. You can do it ! What is your new guess?  ")
+#        print(tryagain)
+#        if str(number) > tryagain: break
+#        print("Too low now ugh !")
+#        if str(number) == tryagain:
+#            print("You did it ! ")
+#elif userguess < str(number):
+#    print("Doooohhhh ! That number is too low !")
+#    print(number)
+#elif userguess == str(number):
+#    print("Are you a mind reader?!")
+#    print(number)
 
 
 
